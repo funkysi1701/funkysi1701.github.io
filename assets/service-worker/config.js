e434927a212310fa8bf4142b4bb37c136a37bc36
Simon Foster <funkysi1701@gmail.com>
2026-06-01 13:54:03 +0100

    more

---
+++ assets/service-worker/config.js
@@ -0,0 +1,31 @@
+{{- $defaultRivision := now.Unix  -}}
+{{- $pages := slice -}}
+{{- range hugo.Sites -}}
+  {{- range .Pages -}}
+    {{- $revision := $defaultRivision -}}
+    {{- with .File -}}
+        {{- $revision = .UniqueID -}}
+    {{- end -}}
+    {{- $pages = $pages | append (dict "url" .Permalink "revision" $revision) -}}
+  {{- end -}}
+{{- end -}}
+
+{{- partial "helpers/read-dir" (dict "Path" "/static" "Scratch" $.Scratch) -}}
+
+{{ if eq (len hugo.Sites) 1 }}
+  {{- $pages = $pages | append (dict "url" (printf "/%s" "manifest.json" | absURL) "revision" $defaultRivision)  -}}
+{{ else }}
+{{- range hugo.Sites -}}
+  {{- $pages = $pages | append (dict "url" (printf "/%s/%s" .Language.Lang "manifest.json" | absURL) "revision" $defaultRivision)  -}}
+{{- end -}}
+{{ end }}
+
+const pages = JSON.parse('{{ $pages | jsonify }}');
+const assets = JSON.parse('{{ $.Scratch.Get "hbs-assets" | jsonify }}');
+const multilingual = {{ if eq (len hugo.Sites) 1 }}false{{ else }}true{{ end }};
+const config = {
+    version: {{ now.Unix }},
+    multilingual: {{ if eq (len hugo.Sites) 1 }}false{{ else }}true{{ end }},
+    pages: pages,
+    assets: assets
+};
